4870b288421759c733ee0df87454002326a6c27d
Nazar-Sergeev <privat.nazar@gmail.com>
2021-07-24 23:39:52 +0300

    init

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserModel} from "../models/UserModel";
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+
+  @Input()
+  user: UserModel;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
