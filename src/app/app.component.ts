1d62210e51f1965e3a360ba19351a7835001938b
Nazar-Sergeev <privat.nazar@gmail.com>
2021-07-29 21:28:04 +0300

    init empty-master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {UserService} from "./services/user.service";
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,14 @@ export class AppComponent {
   title = 'apr2021Angular';
 
 
+  // private userService: UserService;
+  // constructor(userService: UserService) {
+  //   this.userService = userService;
+  //   this.userService.doSomeFun();
+  // }
+
+
+  // constructor(private userService:UserService) {
+  //   this.userService.doSomeFun();
+  // }
 }
